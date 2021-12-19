c7be4a7cfe644f0a8939ad9c81cbe8983491bee4
yanQval <yanqval@gmail.com>
2021-12-20 01:16:45 +0800

    qmix without state

--- src/controllers/cgmix_controller.py
+++ src/controllers/cgmix_controller.py
@@ -12,6 +12,7 @@ from random import randrange
 from components.episode_buffer import EpisodeBatch
 from modules.mixers.vdn import VDNMixer
 from modules.mixers.qmix import QMixer
+from modules.mixers.qmix_nostate import QMixer_wos
 
 
 class CgmixMAC(BasicMAC):
@@ -40,7 +41,7 @@ class CgmixMAC(BasicMAC):
             if args.mixer == "vdn":
                 self.mixer = VDNMixer()
             elif args.mixer == "qmix":
-                self.mixer = QMixer(args)
+                self.mixer = QMixer_wos(args)
             else:
                 raise ValueError("Mixer {} not recognised.".format(args.mixer))
         self.leaky_alpha = args.leaky_alpha
