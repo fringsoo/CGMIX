e189d66c23b79ce86a7a1594a5337c460e2c3f1f
yanQval <yanqval@gmail.com>
2021-12-31 23:32:13 +0800

    fix bugs

--- src/modules/mixers/qmix_wb.py
+++ src/modules/mixers/qmix_wb.py
@@ -50,7 +50,7 @@ class QMixer_wb(nn.Module):
         w1 = w1.view(-1, (self.n_agents + self.n_agents ** 2) // 2, self.embed_dim)
         b1 = b1.view(-1, 1, self.embed_dim)
         # hidden = self.leakyrelu(th.bmm(agent_qs, w1)) # ReLU instead of elu
-        hidden = F.relu(th.bmm(agent_qs, w1) + b1) # ReLU instead of elu
+        hidden = F.elu(th.bmm(agent_qs, w1) + b1) # ReLU instead of elu
         # Second layer
         w_final = th.abs(self.hyper_w_final(states))
         w_final = w_final.view(-1, self.embed_dim, 1)
