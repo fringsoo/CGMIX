61ed4cf5a67e4d1a30f2cdcfe89edfa7c2a515a6
yanQval <yanqval@gmail.com>
2022-01-08 19:42:27 +0800

    fix bugs

--- src/utils/greedy.cpp
+++ src/utils/greedy.cpp
@@ -197,17 +197,18 @@ class GreedyActionSelector
         }
 
         for (int i = 0; i < len; i++)
-            is_on[i] = 1;
-        double w_tot = 0;
-        for (int i = 0; i < len; i++)
-            w_tot += w_final[i];
-        for (int i = 0; i < n; i++)
-            for (int j = i + 1; j < n; j++) if (_id[i][j] > 0)
-                for (int k = 0; k < m; k++)
-                    for(int l = 0; l < m; l++){
-                        new_value_f[i][k] = value_f(i + 1, k + 1);
-                        new_value[_id[i][j]][k][l] = value(i + 1, j + 1, k + 1, l + 1);
-                    }
+            is_on[i] = alpha, new_on[i] = 1.;
+        for (int l = 0; l < len; l++){
+            for (int i = 0; i < n; i++)
+                        for (int j = i + 1; j < n; j++) if (_id[i][j] > 0)
+                            for (int k = 0; k < m; k++)
+                                for(int ll = 0; ll < m; ll++){
+                                    new_value_f[i][k] += value_f(i + 1, k + 1) * (new_on[l] - is_on[l]) * w_final[l];
+                                    new_value[_id[i][j]][k][ll] = value(i + 1, j + 1, k + 1, ll + 1) * (new_on[l] - is_on[l]) * w_final[l];
+                                }
+            is_on[l] = new_on[l];
+        }
+
         for (int iter = 0; iter < 50; iter++){ //// 50 times
 
             memset(vi, 0, sizeof(vi));
@@ -237,9 +238,9 @@ class GreedyActionSelector
                     for (int i = 0; i < n; i++)
                         for (int j = i + 1; j < n; j++) if (_id[i][j] > 0)
                             for (int k = 0; k < m; k++)
-                                for(int l = 0; l < m; l++){
-                                    new_value_f[i][k] += value_f(i + 1, k + 1) * (new_on[l] - is_on[l]) * (w_final[l] / w_tot);
-                                    new_value[_id[i][j]][k][l] = value(i + 1, j + 1, k + 1, l + 1) * (new_on[l] - is_on[l]) * (w_final[l] / w_tot);
+                                for(int ll = 0; ll < m; ll++){
+                                    new_value_f[i][k] += value_f(i + 1, k + 1) * (new_on[l] - is_on[l]) * w_final[l];
+                                    new_value[_id[i][j]][k][ll] = value(i + 1, j + 1, k + 1, ll + 1) * (new_on[l] - is_on[l]) * w_final[l];
                                 }
                     is_on[l] = new_on[l];
                 }
